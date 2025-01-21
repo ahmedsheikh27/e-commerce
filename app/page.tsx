850c9106e8ce6ab6b7168668d6482fdfc5b542dc
ahmedsheikh27 <932ahmedsheikh@gmail.com>
2025-01-21 18:47:16 +0500

    Remove unused Stripe imports and simplify StripeForm component

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,7 @@
 "use client";
-
-import { useState, useEffect } from "react";
-import { loadStripe } from "@stripe/stripe-js";
-import { Elements } from "@stripe/react-stripe-js";
-import CompletePage from "@/components/CompletePage";
-import CheckoutForm from "@/components/CheckoutForm";
 import ProductPage from "./products/page";
 
-
-// Make sure to call loadStripe outside of a component’s render to avoid
-// recreating the Stripe object on every render.
-// This is your test publishable API key.
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
-
 export default function StripeForm() {
-
 
   return (
     <div className="App">
