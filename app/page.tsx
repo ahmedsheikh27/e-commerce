3faac3331133a96a1468b6c9260fe9962a11e041
ahmedsheikh27 <932ahmedsheikh@gmail.com>
2025-01-21 18:44:17 +0500

    Refactor layout and integrate Stripe for payment processing

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,24 @@
-'use client'
+"use client";
+
+import { useState, useEffect } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import CompletePage from "@/components/CompletePage";
+import CheckoutForm from "@/components/CheckoutForm";
 import ProductPage from "./products/page";
 
-export default function Home() {
+
+// Make sure to call loadStripe outside of a component’s render to avoid
+// recreating the Stripe object on every render.
+// This is your test publishable API key.
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
+
+export default function StripeForm() {
+
+
   return (
-    <>
-      <ProductPage />
-    </>
-    );
+    <div className="App">
+      <ProductPage/>
+    </div>
+  );
 }
